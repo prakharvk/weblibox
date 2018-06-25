948e902ea6f8dea47718742f80c3124b35608e82
Prakhar Pandey <prakharpandeyvk@gmail.com>
2018-06-26 00:06:12 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,30 @@
+<!-- Header -->
+
+<header>
+        <nav class="navbar navbar-expand-lg #ffffff white fixed-top scrolling-navbar webli-navbar">
+            <div class="col-md-2 webli-logo" style="padding: 0px;">
+                <img src="img/weblibox.png" style="width:70%;height: 100%;">
+            </div>
+            <div class="col-md-8 antisearch-field">
+                <ul class="webli-navlinks">
+                    <li class="webli-navlinks-li webli-navlinks-li1"><a href="index.php">HOME</a>
+                    </li>
+                    <li class="webli-navlinks-li webli-navlinks-li2"><a href="take-guidance.php">TAKE GUIDANCE</a>
+                    </li>
+                    <li class="webli-navlinks-li webli-navlinks-li3"><a href="buy-rent-books.php">BUY/RENT BOOKS</a>
+                    </li>
+                    <li class="webli-navlinks-li webli-navlinks-li4"><a href="#guidance">HOW IT WORKS</a></li>
+                    <li class="webli-navlinks-li webli-navlinks-li5"><a href="#contact">CONTACT US</a></li>
+                    <li class="webli-navlinks-li webli-navlinks-li5"><a href="blog.php">BLOG</a></li>
+		    <li class="webli-navlinks-li webli-navlinks-li5"><a href="FAQs.php">FAQs</a></li>
+                </ul>
+            </div>
+            <input class="col-md-7 search-field" type="text" name="search" placeholder="search anything..">
+            <div class="col-md-3 webli-navicons col-sm-12">
+                <a class="searcha" style="color: #FF6000;" onclick="searchfield()"><i class="fa fa-search" aria-hidden="true"></i></a>
+                <a style="color: #FF6000;" href="profile.php"><i class="fa fa-user" aria-hidden="true"></i></a>
+                <a style="color: #FF6000;" data-toggle="modal" data-target="#whistlist"><i class="fa fa-heart" aria-hidden="true"></i></a>
+                <a style="color: #FF6000;" data-toggle="modal" data-target="#cart"><i class="fa fa-cart-plus" aria-hidden="true"></i></a>
+            </div>
+        </nav>
+    </header>
